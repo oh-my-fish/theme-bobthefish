1f96c8ffabbee61f31425739e94ccb3140766afc
Justin Hileman <justin@justinhileman.info>
2016-02-19 22:47:57 -0800

    Add last command duration to the right prompt.
     * only if it was > 100ms
     * in human friendly units
     * and make it pop if > 60s
    
    Fixes #21

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,9 +1,36 @@
-# You can override some default title options in your config.fish:
+# You can override some default right prompt options in your config.fish:
 #     set -g theme_date_format "+%a %H:%M"
 
-function fish_right_prompt -d 'bobthefish is all about the right prompt'
+function __bobthefish_cmd_duration -S -d 'Show command duration'
+  [ "$CMD_DURATION" -lt 100 ]; and return
+
+  if [ "$CMD_DURATION" -lt 5000 ]
+    echo -ns $CMD_DURATION 'ms '
+  else if [ "$CMD_DURATION" -lt 60000 ]
+    echo -ns (echo "scale=1;$CMD_DURATION/1000" | bc | sed 's/.0//') 's '
+  else if [ "$CMD_DURATION" -lt 3600000 ]
+    set_color $fish_color_error
+    echo -ns (echo "scale=1;$CMD_DURATION/60000" | bc | sed 's/.0//') 'm '
+  else
+    set_color $fish_color_error
+    echo -ns (echo "scale=1;$CMD_DURATION/3600000" | bc | sed 's/.0//') 'h '
+  end
+
   set_color $fish_color_autosuggestion[1]
+  echo -n $__bobthefish_left_arrow_glyph
+end
+
+function __bobthefish_timestamp -S -d 'Show the current timestamp'
   set -q theme_date_format; or set -l theme_date_format "+%c"
+  echo -n ' '
   date $theme_date_format
-  set_color normal
+end
+
+function fish_right_prompt -d 'bobthefish is all about the right prompt'
+  set -l __bobthefish_left_arrow_glyph \uE0B3
+
+  set_color $fish_color_autosuggestion[1]
+
+  __bobthefish_cmd_duration
+  __bobthefish_timestamp
 end
