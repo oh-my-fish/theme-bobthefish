0ab6f79efe6c8a500c7c51a9d566dbca119ca4c9
Pavle Portic <pavle.portic@gmail.com>
2016-03-03 03:40:04 +0100

    Add show error code option

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -30,6 +30,7 @@
 #     set -g theme_avoid_ambiguous_glyphs yes
 #     set -g theme_nerd_fonts yes
 #     set -g default_user your_normal_user
+#     set -g show_error_code yes
 
 # ===========================
 # Helper methods
@@ -267,7 +268,11 @@ function __bobthefish_prompt_status -S -a last_status -d 'Display symbols for a 
     __bobthefish_start_segment fff 000
     if [ "$nonzero" ]
       set_color $__bobthefish_med_red --bold
-      echo -n $__bobthefish_nonzero_exit_glyph
+      if [ "$show_error_code" = 'yes' ]
+      	echo -n $last_status' '
+      else
+      	echo -n $__bobthefish_nonzero_exit_glyph
+      end
     end
 
     if [ "$superuser" ]
