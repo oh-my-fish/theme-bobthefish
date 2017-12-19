201309db80ecae1b996519414928bfe40893ab65
Justin Hileman <justin@justinhileman.info>
2017-12-19 12:21:28 -0800

    Don't set -g color schemes.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1382,29 +1382,29 @@ function fish_prompt -d 'bobthefish, a fish theme optimized for awesome'
       set -l fg       fbf1c7 ebdbb2 d5c4a1 a89984
       set -l bg       504945 282828
 
-      set -g __color_initial_segment_exit  $fg[1] $red[2] --bold
-      set -g __color_initial_segment_su    $fg[1] $green[2] --bold
-      set -g __color_initial_segment_jobs  $fg[1] $aqua[2] --bold
+      set __color_initial_segment_exit  $fg[1] $red[2] --bold
+      set __color_initial_segment_su    $fg[1] $green[2] --bold
+      set __color_initial_segment_jobs  $fg[1] $aqua[2] --bold
 
-      set -g __color_path                  $bg[1] $fg[2]
-      set -g __color_path_basename         $bg[1] $fg[2] --bold
-      set -g __color_path_nowrite          $red[1] $fg[2]
-      set -g __color_path_nowrite_basename $red[1] $fg[2] --bold
+      set __color_path                  $bg[1] $fg[2]
+      set __color_path_basename         $bg[1] $fg[2] --bold
+      set __color_path_nowrite          $red[1] $fg[2]
+      set __color_path_nowrite_basename $red[1] $fg[2] --bold
 
-      set -g __color_repo                  $green[2] $bg[1]
-      set -g __color_repo_work_tree        $bg[1] $fg[2] --bold
-      set -g __color_repo_dirty            $red[2] $fg[2]
-      set -g __color_repo_staged           $yellow[1] $bg[1]
+      set __color_repo                  $green[2] $bg[1]
+      set __color_repo_work_tree        $bg[1] $fg[2] --bold
+      set __color_repo_dirty            $red[2] $fg[2]
+      set __color_repo_staged           $yellow[1] $bg[1]
 
-      set -g __color_vi_mode_default       $fg[4] $bg[2] --bold
-      set -g __color_vi_mode_insert        $blue[1] $bg[2] --bold
-      set -g __color_vi_mode_visual        $yellow[1] $bg[2] --bold
+      set __color_vi_mode_default       $fg[4] $bg[2] --bold
+      set __color_vi_mode_insert        $blue[1] $bg[2] --bold
+      set __color_vi_mode_visual        $yellow[1] $bg[2] --bold
 
-      set -g __color_vagrant               $blue[2] $fg[2] --bold
-      set -g __color_username              $fg[3] $blue[2] --bold
-      set -g __color_hostname              $fg[3] $blue[2]
-      set -g __color_rvm                   $red[2] $fg[2] --bold
-      set -g __color_virtualfish           $blue[2] $fg[2] --bold
+      set __color_vagrant               $blue[2] $fg[2] --bold
+      set __color_username              $fg[3] $blue[2] --bold
+      set __color_hostname              $fg[3] $blue[2]
+      set __color_rvm                   $red[2] $fg[2] --bold
+      set __color_virtualfish           $blue[2] $fg[2] --bold
 
     case '*' # default dark theme
       #               light  medium dark
