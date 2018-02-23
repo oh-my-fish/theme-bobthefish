11786eab9bdc1be4478c218ced514dc9b51891b9
Philip Fulgham <philfulgham@gmail.com>
2018-02-22 19:59:20 -0500

    Fix issue with filtering out +0/-0

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -244,7 +244,7 @@ function __bobthefish_git_dirty_verbose -S -d 'Print a more verbose dirty state 
   set -l changes (command git diff --numstat | awk '{ added += $1; removed += $2 } END { print "+" added "/-" removed }')
     or return
 
-  echo "$changes " | string replace '+0/' '' | string replace '/-0 ' ' '
+  echo "$changes " | string replace '+0/-0 ' '' | string replace '+0/' '' | string replace '/-0 ' ' '
 end
 
 # ==============================
