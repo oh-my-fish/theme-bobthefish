ed3ca3d618a078775a157866150cfbbe649044fd
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-06-02 18:27:26 +1000

    added support for conda environments. (#150)
    * added support for conda environments.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -682,14 +682,18 @@ function __bobthefish_virtualenv_python_version -S -d 'Get current Python versio
   end
 end
 
-function __bobthefish_prompt_virtualfish -S -d "Display current Python virtual environment (only for virtualfish, virtualenv's activate.fish changes prompt by itself)"
-  [ "$theme_display_virtualenv" = 'no' -o -z "$VIRTUAL_ENV" ]; and return
+function __bobthefish_prompt_virtualfish -S -d "Display current Python virtual environment (only for virtualfish, virtualenv's activate.fish changes prompt by itself) or conda environment."
+  [ "$theme_display_virtualenv" = 'no' -o -z "$VIRTUAL_ENV" -a -z "$CONDA_DEFAULT_ENV" ]; and return
   set -l version_glyph (__bobthefish_virtualenv_python_version)
   if [ "$version_glyph" ]
     __bobthefish_start_segment $color_virtualfish
     echo -ns $virtualenv_glyph $version_glyph ' '
   end
-  echo -ns (basename "$VIRTUAL_ENV") ' '
+  if [ "$VIRTUAL_ENV" ]
+    echo -ns (basename "$VIRTUAL_ENV") ' '
+  else if [ "$CONDA_DEFAULT_ENV" ]
+    echo -ns (basename "$CONDA_DEFAULT_ENV") ' '
+  end
 end
 
 function __bobthefish_prompt_virtualgo -S -d 'Display current Go virtual environment'
