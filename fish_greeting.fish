e31962395f3b90ce12c1d185c9bf2e4671ddc5f0
Justin Hileman <justin@justinhileman.info>
2019-01-27 01:19:39 -0800

    Preserve compatibility with fish <2.5

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,6 +1,10 @@
 function fish_greeting -d "What's up, fish?"
     set_color $fish_color_autosuggestion
     uname -nmsr
-    command -q -s uptime; and uptime
+
+    # TODO: `command -q -s` only works on fish 2.5+, so hold off on that for now
+    command -s uptime >/dev/null
+    and uptime
+
     set_color normal
 end
