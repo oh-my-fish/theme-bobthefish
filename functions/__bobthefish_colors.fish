10b19564aea2c4a5d87f25c40723d61b706bda99
Marc <39213657+marcplustwo@users.noreply.github.com>
2020-01-21 14:53:55 +0100

    fix indents, color contrast

--- functions/__bobthefish_colors.fish
+++ functions/__bobthefish_colors.fish
@@ -482,27 +482,27 @@ function __bobthefish_colors -S -a color_scheme -d 'Define colors used by bobthe
 
     case 'nord'
       set -l base00  2E3440
-	  set -l base01  3B4252
-	  set -l base02  434C5E
-	  set -l base03  4C566A
-	  set -l base04  D8DEE9
-	  set -l base05  E5E9F0
-	  set -l base06  ECEFF4
-	  set -l base07  8FBCBB
-	  set -l base08  88C0D0
-	  set -l base09  81A1C1
-	  set -l base0A  5E81AC
-	  set -l base0B  BF616A
-	  set -l base0C  D08770
-	  set -l base0D  EBCB8B
-	  set -l base0E  A3BE8C
-	  set -l base0F  B48EAD
+      set -l base01  3B4252
+      set -l base02  434C5E
+      set -l base03  4C566A
+      set -l base04  D8DEE9
+      set -l base05  E5E9F0
+      set -l base06  ECEFF4
+      set -l base07  8FBCBB
+      set -l base08  88C0D0
+      set -l base09  81A1C1
+      set -l base0A  5E81AC
+      set -l base0B  BF616A
+      set -l base0C  D08770
+      set -l base0D  EBCB8B
+      set -l base0E  A3BE8C
+      set -l base0F  B48EAD
 
       set -l colorfg $base00
 
       set -x color_initial_segment_exit     $base05 $base08 --bold
       set -x color_initial_segment_su       $base05 $base0B --bold
-      set -x color_initial_segment_jobs     $base05 $base0C --bold
+      set -x color_initial_segment_jobs     $base08 $base0D --bold
 
       set -x color_path                     $base02 $base05
       set -x color_path_basename            $base02 $base06 --bold
@@ -522,11 +522,11 @@ function __bobthefish_colors -S -a color_scheme -d 'Define colors used by bobthe
       set -x color_k8s                      $base02 $colorfg --bold
       set -x color_username                 $base02 $base0D --bold
       set -x color_hostname                 $base02 $base0D
-      set -x color_rvm                      $base08 $colorfg --bold
-      set -x color_nvm                      $base08 $colorfg --bold
-      set -x color_virtualfish              $base08 $colorfg --bold
-      set -x color_virtualgo                $base08 $colorfg --bold
-      set -x color_desk                     $base08 $colorfg --bold
+      set -x color_rvm                      $base09 $colorfg --bold
+      set -x color_nvm                      $base09 $colorfg --bold
+      set -x color_virtualfish              $base09 $colorfg --bold
+      set -x color_virtualgo                $base09 $colorfg --bold
+      set -x color_desk                     $base09 $colorfg --bold
 
     case '*' # default dark theme
       #               light  medium dark
