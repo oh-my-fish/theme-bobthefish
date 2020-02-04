7dd8f19216e6c00aee793c670ce5668d45254c6c
leyla <leyla@astralfield.blue>
2020-02-04 11:38:45 +0900

    fix

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -509,7 +509,7 @@ end
 # ==============================
 
 function __bobthefish_prompt_aws_account_id -S -d 'Display AWS AccountId'
-    function get-account-auto -S --on-variable AWS_ACCESS_KEY_ID
+    function get-account-auto -S --on-variable AWS_SESSION_TOKEN
         set -x AWS_ACCOUNT_ID (aws sts get-caller-identity --output text --query "Account")
     end
     [ "$theme_display_aws_account_id" = 'no' -o -n "$AWS_ACCOUNT_ID" ]; or return
