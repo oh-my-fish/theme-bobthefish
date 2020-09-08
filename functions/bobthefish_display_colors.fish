bc545a92cbea1d0656227e4ea675bcba8b57e8ee
Darren Kidd <dev@kidd.life>
2020-09-08 15:30:18 +1200

    Starting with custom colours!
    * Just copying the k8s theme at the moment.
    * Added in a placeholder to bobthefish_display_colors() so I can check theming easily.

--- functions/bobthefish_display_colors.fish
+++ functions/bobthefish_display_colors.fish
@@ -141,5 +141,9 @@ function bobthefish_display_colors -a color_scheme -d 'Print example prompt colo
   echo -ns $desk_glyph desk ' '
   __bobthefish_finish_segments
 
+  __bobthefish_start_segment $color_aws_vault
+  echo -ns $aws_vault_glyph ' aws-vault '
+  __bobthefish_finish_segments
+
   echo -e "\n"
 end
