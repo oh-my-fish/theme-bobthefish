dafe60691635f1ae2a6aa9bb1c5516bfa0e04413
Darren Kidd <dev@kidd.life>
2020-09-09 08:11:10 +1200

    Revert "Custom glyph ☁"
    This reverts commit 398a60b8663f0be26563fd5b05e688b8bdb0b146.

--- functions/__bobthefish_glyphs.fish
+++ functions/__bobthefish_glyphs.fish
@@ -30,9 +30,6 @@ function __bobthefish_glyphs -S -d 'Define glyphs used by bobthefish'
   # Kubernetes glyphs
   set -x k8s_glyph               \u2388 # '⎈'
 
-  # AWS glyphs
-  set -x aws_vault_glyph         \u2601 # '☁'
-
   # Vagrant glyphs
   set -x vagrant_running_glyph   \u2191 # ↑ 'running'
   set -x vagrant_poweroff_glyph  \u2193 # ↓ 'poweroff'
