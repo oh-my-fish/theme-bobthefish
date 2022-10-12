9ff7cd3f3c1445830895ca3b02a741a0e8692a5e
Sean Patrick Hagen <sean.hagen@gmail.com>
2022-10-12 14:05:37 -0700

    Remove backslash to remove grep warning
    New in grep 3.8 apparently is this warning: `grep: warning: stray \ before white
    space`.
    
    Explanation: https://github.com/koalaman/shellcheck/issues/2573

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -857,7 +857,7 @@ function __bobthefish_prompt_golang -S -a real_pwd -d 'Display current Go inform
     set -l d $real_pwd
     while not [ -z "$d" ]
         if [ -e $d/go.mod ]
-            grep "^go\ " "$d/go.mod" | read __ gomod_version
+            grep "^go " "$d/go.mod" | read __ gomod_version
             break
         end
 
