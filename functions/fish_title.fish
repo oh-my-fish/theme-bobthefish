76cac812064fa749ffc258a20398c6f6250860c5
Justin Hileman <justin@justinhileman.info>
2023-03-26 10:22:17 -0400

    Don't overwrite $hostname
    Fixes #339

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -8,8 +8,8 @@ function __bobthefish_title_user -S -d 'Display actual user if different from $d
     if [ "$theme_title_display_user" = 'yes' ]
         if [ "$USER" != "$default_user" -o -n "$SSH_CLIENT" ]
             set -l IFS .
-            uname -n | read -l hostname __
-            echo -ns (whoami) '@' $hostname ' '
+            uname -n | read -l host __
+            echo -ns (whoami) '@' $host ' '
         end
     end
 end
